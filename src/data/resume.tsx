21ef33ce6475b3241f83e9f0efd8aaebc41581c0
awal.deriyanto <awal.deriyanto@delameta.id>
2025-05-31 07:43:24 +0700

    docs: add profile

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,15 +2,15 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
-  initials: "DV",
-  url: "https://dillion.io",
-  location: "San Francisco, CA",
-  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  name: "Awal Deriyanto",
+  initials: "AD",
+  url: "https://awalnur.me",
+  location: "East Jakarta, Indonesia",
+  locationLink: "https://www.google.com/maps/place/East+Jakarta",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "Software Engineer as specialist in backend and infrastructure. I love building things and helping people.",
   summary:
-    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+    "At 2017, I started my career as a software engineer. I start from a small company in Indonesia and grow up to a big company in Indonesia. I have experience in building and scaling a company from the ground up.",
   avatarUrl: "/me.png",
   skills: [
     "Python",
